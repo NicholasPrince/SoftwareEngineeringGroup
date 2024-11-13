e81e92b379dac469620772ec5eed250f69451902
dylanchristensen <r.dylan.christensen@gmail.com>
2024-11-13 16:12:51 -0500

    forgot a couple files

---
+++ future/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_content_view, name='home'),           # Landing page
+    path('path/', views.view_path_view, name='view_path'),    # View Path page
+    path('progress/', views.progress_view, name='progress'),  # Progress page
+    path('login/', views.login_view, name='login'),           # Login page
+    path('register/', views.register_view, name='register'),  # Register page
+]
